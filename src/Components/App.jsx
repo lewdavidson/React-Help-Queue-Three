0d64d35c105b15b05cb28f6e1daeb809c5f5bd45
lewdavidson <lewdavidson47@gmail.com>
2018-02-11 21:10:22 -0800

    add states and unique ids to affect the flow of data

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
+class App extends React.Component {
+
+  constructior(props) {
+    super(props);
+    this.state = {
+      masterTicketList : []
+    };
+    this.handleAddingNewTicketToList = this.HandleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket) {
+    const newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+
+  }
   return (
     <div>
       <Header />
-      <TicketList />
+      <Switch>
+        <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+        <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAdddingNewTicketToList} />} />
+        <Route component={Error404} />
+      </Switch>
     </div>
   );
 }
